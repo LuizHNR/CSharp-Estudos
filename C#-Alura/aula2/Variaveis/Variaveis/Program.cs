8f66a7739441567b17d9f9874e5d097d409bd787
LuizHNR <luizhneri12@gmail.com>
2024-08-10 21:38:22 -0300

    Variaveis

---
+++ C#-Alura/aula2/Variaveis/Variaveis/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Aula2
+{
+    static void Main()
+    {
+        byte n1 = 10; // entre 0 e 255
+        int num = -20;
+        char letra = 'c';// só um caracter
+        float valor = 5.4f; //f para mostrar q é float
+        string nome = "Luiz";
+
+        var aux = 10;
+
+        Console.WriteLine(aux);
+    }
+}
---
+++ teste/Aula2/Aula02.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class Aula2{
+
+    static void Main(){
+
+    // Tipo/Nome
+        byte n1=10; // entre 0 e 255
+        int num=-10;
+        char letra='c'; // somente um caracter
+        float valor=10.5f;  //f para relacionar a float
+        string nome="Luiz";
+
+      //Declarar multiplas variaveis  
+        int num1,num2,res; // e pode tbm num1 = 10, num2 = 20, res = num1 * num2;
+        num1 = 10;
+        num2 = 2;
+        res = num1 * num2;
+
+        var aux=10; // Não expecificado por nos, mas o sistema q define
+
+        Console.WriteLine("Valor da variavel: " + aux);
+
+        Console.WriteLine("A Multiplicação de " + num1 + " x " + num2 + " é igual a: " + res); // ou num1 * num2
+    }
+}
