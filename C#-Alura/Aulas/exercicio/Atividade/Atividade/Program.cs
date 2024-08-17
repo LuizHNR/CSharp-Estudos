14d4acb6bc1becea18dbc1e31f16cf7c810c9b82
LuizHNR <luizhneri12@gmail.com>
2024-08-17 18:08:44 -0300

    atividade

---
+++ C#-Alura/Aulas/exercicio/Atividade/Atividade/Program.cs
@@ -0,0 +1,10 @@
+﻿int notaMedia = 3;
+
+if (notaMedia >= 5)
+{
+    Console.WriteLine($"{notaMedia} Nota o suficiente para aprovação");
+}
+else
+{
+    Console.WriteLine("Nota insuficiente para aprovação");
+}
