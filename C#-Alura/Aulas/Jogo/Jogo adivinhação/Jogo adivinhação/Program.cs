d31f996a4beb260268bcc27ef17184af847433d6
LuizHNR <luizhneri12@gmail.com>
2024-08-17 18:13:17 -0300

    jogo completo

---
+++ C#-Alura/Aulas/Jogo/Jogo adivinhação/Jogo adivinhação/Program.cs
@@ -0,0 +1,25 @@
+﻿Random aleatorio = new Random();
+int numeroSecreto = aleatorio.Next(1, 101);
+
+do
+{
+    Console.Write("Digite um número entre 1 e 100: ");
+    int chute = int.Parse(Console.ReadLine());
+
+    if (chute == numeroSecreto)
+    {
+        Console.WriteLine("Parabéns! Você acertou o número.");
+        break;
+    }
+    else if (chute < numeroSecreto)
+    {
+        Console.WriteLine("O número é maior.");
+    }
+    else
+    {
+        Console.WriteLine("O número é menor.");
+    }
+
+} while (true);
+
+Console.WriteLine("O jogo acabou. Você acertou o número secreto!");
