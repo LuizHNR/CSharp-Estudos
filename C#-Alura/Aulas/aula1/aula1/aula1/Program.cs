44a804f07facdd940591b9d5b7f2346ecd88e4a5
LuizHNR <luizhneri12@gmail.com>
2024-08-17 17:14:25 -0300

    Função

---
+++ C#-Alura/Aulas/aula1/aula1/aula1/Program.cs
@@ -0,0 +1,21 @@
+﻿// Screen Sound
+string mensagemDeBoasVindas = "Boas vindas GuruGuru";
+
+//void é uma função q não devolve nada, sem return
+void ExibirMensagemDeBoasVindas()
+{
+    //@ é o Verbatim Literal que é colocar a string do jeito q vc quer q apareça
+    Console.WriteLine(@"
+    
+░██████╗░██╗░░░██╗██████╗░██╗░░░██╗░██████╗░██╗░░░██╗██████╗░██╗░░░██╗
+██╔════╝░██║░░░██║██╔══██╗██║░░░██║██╔════╝░██║░░░██║██╔══██╗██║░░░██║
+██║░░██╗░██║░░░██║██████╔╝██║░░░██║██║░░██╗░██║░░░██║██████╔╝██║░░░██║
+██║░░╚██╗██║░░░██║██╔══██╗██║░░░██║██║░░╚██╗██║░░░██║██╔══██╗██║░░░██║
+╚██████╔╝╚██████╔╝██║░░██║╚██████╔╝╚██████╔╝╚██████╔╝██║░░██║╚██████╔╝
+░╚═════╝░░╚═════╝░╚═╝░░╚═╝░╚═════╝░░╚═════╝░░╚═════╝░╚═╝░░╚═╝░╚═════╝░
+");
+   
+    Console.WriteLine(mensagemDeBoasVindas);
+}
+
+ExibirMensagemDeBoasVindas();
--- C#-Alura/aula1/exercicio1/exercicio1/Program.cs
+++
@@ -1,8 +0,0 @@
-﻿string curso = "CSharp";
-
-Console.WriteLine(curso);
-
-string nome = "Luiz";
-string sobrenome = "Henrique";
-
-Console.WriteLine("Luiz Henrique");
