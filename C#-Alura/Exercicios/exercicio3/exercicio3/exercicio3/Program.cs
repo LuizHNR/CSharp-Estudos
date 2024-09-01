2bba9c890f83ebe060f7ec53a7944d97c75ebe8a
LuizHNR <luizhneri12@gmail.com>
2024-08-31 23:08:43 -0300

    Exercicio

---
+++ C#-Alura/Exercicios/exercicio3/exercicio3/exercicio3/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Hello, World!");
+
+var alunos = new Dictionary<string, List<int>> {
+    { "Luiz", new List<int>{ 10,7,10 } }
+};
+
+List<int> notas = alunos["Luiz"];
+double mediaAluno = notas.Average();
+Console.WriteLine($"\n\nA media do aluno é de {mediaAluno}");
