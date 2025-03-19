f537e64ae44f6973ef0a360ef94ecfc184cbb229
LuizHNR <luizhneri12@gmail.com>
2025-03-18 23:16:29 -0300

    Parte 2

---
+++ C#-Alura/Parte2/aula1/EstudoPart2/Musica.cs
@@ -0,0 +1,11 @@
+﻿namespace EstudoPart2;
+
+public class Musica
+{
+    public string nome;
+    public string artista;
+    public int duracao;
+    public bool disponivel;
+    
+    
+}
---
+++ C#-Alura/Parte2/aula1/EstudoPart2/Program.cs
@@ -0,0 +1,17 @@
+﻿using EstudoPart2;
+
+Musica musica1 = new Musica();
+
+musica1.nome = "Roxame";
+musica1.artista = "The Police";
+
+Console.WriteLine($"Nome da Música: {musica1.nome}");
+Console.WriteLine($"Nome do Artista: {musica1.artista}");
+
+Musica musica2 = new Musica();
+
+musica2.nome = "Vertigo";
+musica2.artista  = "U2";
+
+Console.WriteLine($"Nome da Música: {musica2.nome}");
+Console.WriteLine($"Nome do Artista: {musica1.artista}");
