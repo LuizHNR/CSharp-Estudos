3e167c37476c6caaea9789186521f2626c0f2277
LuizHNR <luizhneri12@gmail.com>
2025-03-23 00:17:22 -0300

    Aula 3

---
+++ C#-Alura/Parte2/aula3/aula3/aula3/Album.cs
@@ -0,0 +1,24 @@
+﻿namespace aula3;
+
+class Album
+{
+    private List<Musica> musicas = new List<Musica>();
+    public string Nome { get; set; }
+    public int DuracaoTotal => musicas.Sum(m => m.Duracao);  
+
+    public void AdicionarMusica(Musica musica)
+    {
+        musicas.Add(musica); 
+    }
+
+    public void ExibirMusicasdoAlbum()
+    {
+        foreach (var musica in musicas)
+        {
+            Console.WriteLine($"Musicas do Album {Nome}:");
+            Console.WriteLine($"Musicas: {musica.Nome}\n"); 
+        }
+
+        Console.WriteLine($"Para ouvir esse album inteiro você precisa de: {DuracaoTotal} segundos");
+    }
+}
---
+++ C#-Alura/Parte2/aula3/aula3/aula3/Program.cs
@@ -0,0 +1,28 @@
+﻿using aula3;
+
+Console.WriteLine(@"
+██████╗░░█████╗░███╗░░██╗██████╗░░█████╗░░██████╗
+██╔══██╗██╔══██╗████╗░██║██╔══██╗██╔══██╗██╔════╝
+██████╦╝███████║██╔██╗██║██║░░██║███████║╚█████╗░
+██╔══██╗██╔══██║██║╚████║██║░░██║██╔══██║░╚═══██╗
+██████╦╝██║░░██║██║░╚███║██████╔╝██║░░██║██████╔╝
+╚═════╝░╚═╝░░╚═╝╚═╝░░╚══╝╚═════╝░╚═╝░░╚═╝╚═════╝░");
+
+Console.WriteLine("\n");
+
+Album albumDoQueen = new Album();
+
+albumDoQueen.Nome = "A night at the Opera";
+
+Musica musica1 = new Musica();
+musica1.Nome = "Love of my Life";
+musica1.Duracao = 213;
+
+Musica musica2 = new Musica();
+musica2.Nome = "Bohemian Rhapsoyd";
+musica2.Duracao = 354;
+
+albumDoQueen.AdicionarMusica(musica1);
+albumDoQueen.AdicionarMusica(musica2);
+
+albumDoQueen.ExibirMusicasdoAlbum();
