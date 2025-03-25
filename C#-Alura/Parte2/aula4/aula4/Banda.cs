b11162c6374dda60740b5adf9c5cfc6a9a0d9c77
LuizHNR <luizhneri12@gmail.com>
2025-03-24 23:11:28 -0300

    Parte 1 da aula 4

---
+++ C#-Alura/Parte2/aula4/aula4/Banda.cs
@@ -0,0 +1,21 @@
+﻿namespace aula4;
+
+public class Banda
+{
+    private List<Album> albums = new List<Album>();
+    public string Nome { get; set; }
+
+    public void AdicionarAlbum(Album album)
+    {
+        albums.Add(album);
+    }
+
+    public void ExibiDiscografia()
+    {
+        Console.WriteLine($"Discografia da Banda: {Nome}");
+        foreach (Album album in albums )
+        {
+            Console.WriteLine($"Album: {album.Nome} ({album.DuracaoTotal})");
+        }
+    }
+}
---
+++ C#-Alura/Parte2/aula4/aula4/Program.cs
@@ -0,0 +1,31 @@
+﻿using aula4;
+
+Console.WriteLine(@"
+██████╗░░█████╗░███╗░░██╗██████╗░░█████╗░░██████╗
+██╔══██╗██╔══██╗████╗░██║██╔══██╗██╔══██╗██╔════╝
+██████╦╝███████║██╔██╗██║██║░░██║███████║╚█████╗░
+██╔══██╗██╔══██║██║╚████║██║░░██║██╔══██║░╚═══██╗
+██████╦╝██║░░██║██║░╚███║██████╔╝██║░░██║██████╔╝
+╚═════╝░╚═╝░░╚═╝╚═╝░░╚══╝╚═════╝░╚═╝░░╚═╝╚═════╝░");
+
+Console.WriteLine("\n");
+
+Album albumDoQueen = new Album();
+
+albumDoQueen.Nome = "A night at the Opera";
+
+Musica musica1 = new Musica();
+musica1.Nome = "Love of my Life";
+musica1.Duracao = 213;
+
+Musica musica2 = new Musica();
+musica2.Nome = "Bohemian Rhapsoyd";
+musica2.Duracao = 354;
+
+albumDoQueen.AdicionarMusica(musica1);
+albumDoQueen.AdicionarMusica(musica2);
+
+Banda queen = new Banda();
+queen.Nome = "Queen";
+queen.AdicionarAlbum(albumDoQueen);
+queen.ExibiDiscografia();
